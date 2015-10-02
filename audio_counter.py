6836d378652ab0f20366f543fd5b8cd20ee3e171
Rian Rivz <if.ge@ya.ru>
2015-10-02 03:51:00 +0300

    Практикум №3

--- audio_counter.py
+++ audio_counter.py
@@ -12,7 +12,8 @@ def gen_artist_track(count, dif):
 			for x in range(random.randrange(2,15)):
 				audios.append({'artist':'artist %s' % i,'title':'track %s' % i})
 		else:
-			audios.append({'artist':'artist %s' % i,'title':'track %s' % i})
+			for x in range(i):
+				audios.append({'artist':'artist %s' % i,'title':'track %s' % i})
 
 	return audios
 
@@ -37,14 +38,29 @@ def count_artists(audios, test):
 			test_arr.append({'artist': i, 'count': artists.count(i)})
 		else:
 			print "%s (%s)" % (i, artists.count(i)) 
+	if test: return test_arr
+
+def test_count_artists(audios_count, iterations):
+	"""Фунеция писалась в 4 часа утра. За извращенство понять и простить"""
+	result = False
+	#Пробегаемся по кол-во нужных проверок
+	for iteration in range(iterations):
+		count = audios_count
+		audios = gen_artist_track(count, False) #Генерируем аудио список с выключенной рандомизацией
+		#это означает, что у первого артиста будет 1 трек, у 2-ого два и т.д.
+		count = count_artists(audios, True) # Делаем подсчет с включенным тест режимом (отдает результыт в виде массива, а не выводит на экран)
+
+		#Сама проверка
+		for i in range(audios_count):
+			i+=1
+			for c in count:
+				if c['count'] == i:result = True
+
+	print "%s" % result
 
 
-def test_count_artists(audios, iterations):
-	for iteration in iterations:
-		audios = gen_artist_track(5, True)
 
-		count_artists(audios, True)
-		
+
 
 
 
@@ -56,4 +72,5 @@ audios = gen_artist_track(10, True)
 """Будет ли адекватно писать так?"""
 #display_artist_track(gen_artist_track())
 
-count_artists(audios)
+count_artists(audios, False)
+test_count_artists(5,1)
