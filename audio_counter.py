58182089aac8c40cb952d3968b8ff8090a0744d2
Rian Rivz <if.ge@ya.ru>
2015-10-02 20:14:14 +0300

    Выслушал пожелания, не понял философию подсчета

--- audio_counter.py
+++ audio_counter.py
@@ -1,19 +1,19 @@
 # coding=utf-8
-#audios = ({'artist':'artist1','track':'track1'})
 
 import random
 
-def gen_artist_track(count, dif):
-	audios = []
-	for i in range(count):
-		i+=1
 
-		if dif:
-			for x in range(random.randrange(2,15)):
-				audios.append({'artist':'artist %s' % i,'title':'track %s' % i})
+def gen_artist_track(count, is_use_random_songs_amount=True):
+	audios = []
+	for i in range(1, count + 1):
+
+		if is_use_random_songs_amount:
+			use_range = range(random.randrange(2,15))
 		else:
-			for x in range(i):
-				audios.append({'artist':'artist %s' % i,'title':'track %s' % i})
+			use_range = range(i)
+
+		for x in use_range:
+			audios.append({'artist':'artist %s' % i,'title':'track %s' % i})
 
 	return audios
 
@@ -25,38 +25,21 @@ def display_artist_track(audios):
 			counter + 1, audio['artist'], audio['title']
 		)
 
-def count_artists(audios, test):
-	artists = []
-	for audio in audios:
-		artists.append(audio['artist'])
+
+def count_artists(audios):
+
+	artists = [audio['artist'] for audio in audios]
 
 	artists_set = set(artists)	
-	test_arr = []
+	artists_songs_amount = []
 
 	for i in artists_set:
-		if test:
-			test_arr.append({'artist': i, 'count': artists.count(i)})
-		else:
-			print "%s (%s)" % (i, artists.count(i)) 
-	if test: return test_arr
+		artists_songs_amount.append({'artist': i, 'count': artists.count(i)})
+		print "%s (%s)" % (i, artists.count(i)) 
+
 
 def test_count_artists(audios_count, iterations):
-	"""Фунеция писалась в 4 часа утра. За извращенство понять и простить"""
-	result = False
-	#Пробегаемся по кол-во нужных проверок
-	for iteration in range(iterations):
-		count = audios_count
-		audios = gen_artist_track(count, False) #Генерируем аудио список с выключенной рандомизацией
-		#это означает, что у первого артиста будет 1 трек, у 2-ого два и т.д.
-		count = count_artists(audios, True) # Делаем подсчет с включенным тест режимом (отдает результыт в виде массива, а не выводит на экран)
-
-		#Сама проверка
-		for i in range(audios_count):
-			i+=1
-			for c in count:
-				if c['count'] == i:result = True
-
-	print "%s" % result
+	pass
 
 
 
@@ -65,12 +48,8 @@ def test_count_artists(audios_count, iterations):
 
 
 
-audios = gen_artist_track(10, True)
+audios = gen_artist_track(10)
 
 #display_artist_track(audios)
 
-"""Будет ли адекватно писать так?"""
-#display_artist_track(gen_artist_track())
-
-count_artists(audios, False)
-test_count_artists(5,1)
+count_artists(audios)
